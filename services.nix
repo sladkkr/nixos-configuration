6a2483b7819c6aecce84462fd92b1979b89a5d46
sladkkr <klimczakkarol@outlook.com>
2024-07-15 21:27:44 +0200

    23

--- services.nix
+++ services.nix
@@ -23,50 +23,60 @@
 		xserver.desktopManager.gnome.enable = true;
 	};
 
-	systemd.services.testing-inotifywait = {
-		description = "Testing inotifywait";
-		serviceConfig = {
-			Type = "oneshot";
-			RemainAfterExit = true;
-			ExecStart = pkgs.writeShellScript "testing-inotifywait.sh" ''
-				${pkgs.inotify-tools}/bin/inotifywait  -e create,moved_to,attrib --include '/karol-sync-nixos-configuration-finished$' -qq /tmp && \
-				mv /tmp/karol-sync-nixos-configuration-finished /tmp/systemd-finished
-			'';
+	systemd.services = {
+		sync-nixos-configuration = let 
+			path = "/home/karol/repos/nixos-configuration";
+		in {
+			enable = true;
+			description = "Keeping /home/karol/repos/nixos-configuration in sync with remote repository.";
+			wants = ["NetworkManager-wait-online.service"];
+			after = ["NetworkManager-wait-online.service"];
+			path = with pkgs; [bash git openssh ];
+			serviceConfig = {
+				Type = "oneshot";
+				RemainAfterExit = true;
+				User = "karol";
+				Group = "users";
+				ExecStart = "/usr/bin/env bash /home/karol/.local/bin/git-auto-pull ${path}";
+				ExecStop = "/usr/bin/env bash /home/karol/.local/bin/git-auto-commit ${path}";
+			};
 		};
 	};
 
-	home-manager.users.karol.systemd.user = {
-		enable = true;
-		services = let
-			createSyncService = path: {
-				Unit.Description = "Keeping ${path} in sync with repository.";
-				Install.WantedBy = ["default.target"];
-				Service = {
-					Type = "oneshot";
-					RemainAfterExit = true;
-					Environment = "PATH=/run/current-system/sw/bin/";
-					ExecStartPre = "/usr/bin/env nm-online -sq";
-					ExecStart = "/usr/bin/env bash /home/karol/.local/bin/git-auto-pull ${path}";
-					ExecStop = "/usr/bin/env bash /home/karol/.local/bin/git-auto-commit ${path}";
-				};
-			};
-		in {
-			sync-notes = createSyncService "/home/karol/repos/notes";
-			sync-dotfiles = createSyncService "/home/karol/repos/dotfiles";
-			sync-nixos-configuration = let path = "/home/karol/repos/nixos-configuration"; in {
-				Unit.Description = "Keeping ${path} in sync with repository.";
-				Install.WantedBy = ["default.target"];
-				Service = {
-					Type = "oneshot";
-					RemainAfterExit = true;
-					Environment = "PATH=/run/current-system/sw/bin/";
-					ExecStartPre = "/usr/bin/env nm-online -sq";
-					ExecStart = "/usr/bin/env bash /home/karol/.local/bin/git-auto-pull ${path}";
-					ExecStartPost = "/usr/bin/env touch /tmp/karol-sync-nixos-configuration-finished";
-					ExecStop = "/usr/bin/env bash /home/karol/.local/bin/git-auto-commit ${path}";
-				};
-				
-			};
-		};
-	};
+
+
+	# home-manager.users.karol.systemd.user = {
+	# 	enable = true;
+	# 	services = let
+	# 		createSyncService = path: {
+	# 			Unit.Description = "Keeping ${path} in sync with repository.";
+	# 			Install.WantedBy = ["default.target"];
+	# 			Service = {
+	# 				Type = "oneshot";
+	# 				RemainAfterExit = true;
+	# 				Environment = "PATH=/run/current-system/sw/bin/";
+	# 				ExecStartPre = "/usr/bin/env nm-online -sq";
+	# 				ExecStart = "/usr/bin/env bash /home/karol/.local/bin/git-auto-pull ${path}";
+	# 				ExecStop = "/usr/bin/env bash /home/karol/.local/bin/git-auto-commit ${path}";
+	# 			};
+	# 		};
+	# 	in {
+	# 		sync-notes = createSyncService "/home/karol/repos/notes";
+	# 		sync-dotfiles = createSyncService "/home/karol/repos/dotfiles";
+	# 		sync-nixos-configuration = let path = "/home/karol/repos/nixos-configuration"; in {
+	# 			Unit.Description = "Keeping ${path} in sync with repository.";
+	# 			Install.WantedBy = ["default.target"];
+	# 			Service = {
+	# 				Type = "oneshot";
+	# 				RemainAfterExit = true;
+	# 				Environment = "PATH=/run/current-system/sw/bin/";
+	# 				ExecStartPre = "/usr/bin/env nm-online -sq";
+	# 				ExecStart = "/usr/bin/env bash /home/karol/.local/bin/git-auto-pull ${path}";
+	# 				ExecStartPost = "/usr/bin/env touch /tmp/karol-sync-nixos-configuration-finished";
+	# 				ExecStop = "/usr/bin/env bash /home/karol/.local/bin/git-auto-commit ${path}";
+	# 			};
+	# 			
+	# 		};
+	# 	};
+	# };
 }
